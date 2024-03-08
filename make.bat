d333fe4ba0a572ff2203bc35b2c19a4b63ecdced
ZipiRo <ionutperdun777@gmail.com>
2024-03-08 15:00:00 +0200

    mUpdate

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
-set "dir=source"
-set "code=main.cpp"
-set "name=CharGraph++"
+set "dir=source" :: the folder where the source code is (headers and library)
+set "code=main.cpp" :: the code you want to compile
+set "name=build" :: the final name of the program
 
 g++ %code% %dir%\CharGraph++.a -o %name%
 start cmd /k .\%name%
