845a798706c178647e7d04fe0784c9b0f40c5992
ZipiRo <ionutperdun777@gmail.com>
2024-03-08 15:24:33 +0200

    Update 15:24 08.03.2024

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
-set "dir=source" :: the folder where the source code is (headers and library)
-set "code=main.cpp" :: the code you want to compile
+set "dir=source" :: the path to the directory where the source code is (headers and library)
+set "code=template.cpp" :: the code you want to compile
 set "name=build" :: the final name of the program
 
 g++ %code% %dir%\CharGraph++.a -o %name%
---
+++ template.cpp
@@ -0,0 +1,60 @@
+#include "source\CharGraph++.h"
+
+Scene scene(100, 30, ' ');
+Input input;
+
+int main() {
+	Rectangle rW(1, 1, 10, 5, '#');
+	rW.draw(scene);
+
+	Rectangle rH(22, 1, 5, 10, '#');
+	rH.draw(scene);
+	
+	Squere squere(33, 1, 10, '#');
+	squere.draw(scene);
+
+	Elipse circle(63, 6, 5, 5, '#');
+	circle.draw(scene);
+
+	int x[4] = {74, 84, 94, 85};
+	int y[4] = {6, 1, 6, 10};
+
+	Poligon poligon(x, y, 4, '#');
+	poligon.draw(scene);
+
+	FilledRectangle frW(1, 11, 10, 5, '#', ':');
+	frW.draw(scene);
+
+	FilledRectangle frH(22, 11, 5, 10, '#', ':');
+	frH.draw(scene);
+
+	FilledSquere fs(33, 11, 10, '#', ':');
+	fs.draw(scene);
+
+	FilledElipse fe(63, 16, 5, 5, '#', ':');
+	fe.draw(scene);
+
+	Line l(1, 22, 60, 22, '#');
+	l.draw(scene);
+
+	Point p(63, 22, '#');
+	p.draw(scene);
+
+	char string[100] = "This is a test for the /Dynamic Character Graphics Engine for C++/";
+	Text text(1, 24, string);
+	text.draw(scene);
+
+	Text textnumber(1, 25, 100);
+	textnumber.draw(scene);
+
+	scene.paint();
+	
+	bool running = true;
+	while (running){
+		input.event();
+		if (input.keyPressed(27))
+			running = false;
+	}
+
+	return 0;
+}
