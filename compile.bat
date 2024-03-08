fd48cf0401bc1d0dc7d87cf4917dc302d41f358b
ZipiRo <ionutperdun777@gmail.com>
2024-03-08 13:12:29 +0200

    Update 13:12 08.03.2024

--- compile.bat
+++ compile.bat
@@ -3,4 +3,4 @@ set "dir=source" :: The path where your source code is
 set "toCompile=c_engine.cpp" :: The code you want to compile
 set "buildName=c_engine" :: The final name of the program
 
-g++ %toCompile% %dir%\Elipse.o %dir%\FilledElipse.o %dir%\FilledRectangle.o %dir%\FilledSquere.o %dir%\Input.o %dir%\Line.o %dir%\Point.o %dir%\Poligon.o %dir%\Rectangle.o %dir%\Scene.o %dir%\Squere.o %dir%\Text.o -o %buildName%
+g++ %toCompile% %dir%\c_engine.a -o %buildName%
